778e8cd84fc62124a00dbeff4f76c70678991495
walayatali <walayat.ali@xprolabs.com>
2022-01-28 18:52:38 +0500

    spliting components into multiple components

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,15 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
 const ExpenseItem = (props) => {
-	const month = props.ItemDate.toLocaleString('en-US', { month: 'long' });
-  	const day = props.ItemDate.toLocaleString('en-US', { day: '2-digit' });
-  	const year = props.ItemDate.getFullYear();
 	return (
 		<div className="expense-item">
-			<div className="expense-date">
-				<div className="expense-date__month">{month}</div>
-				<div className="expense-date__year">{year}</div>
-				<div className="expense-date__day">{day}</div>
-			</div>
+			<ExpenseDate ExpDate = {props.ItemDate}/>
 			<div className="expense-item__description">
 				<h2>{props.Title}</h2>
 				<p className="expense-item__price">$ {props.Price}</p>
