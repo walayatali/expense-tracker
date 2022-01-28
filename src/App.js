7b8dc83c256d150807b2215e5155be8b1f00be7b
walayatali <walayat.ali@xprolabs.com>
2022-01-28 18:04:58 +0500

    Passed dynamic items data via props to components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 import ExpenseItem from './components/Expense/ExpenseItem';
 
 function App() {
-  return (
-    <ExpenseItem></ExpenseItem>
-  );
+  
+    const Expenses = [
+        {id: 'e1', ItemDate: new Date(2022, 1, 21), Title: 'Item 1', Price: 19.4},
+        {id: 'e2', ItemDate: new Date(2022, 1, 22), Title: 'Item 2', Price: 31.1},
+        {id: 'e3', ItemDate: new Date(2022, 1, 23), Title: 'Item 3', Price: 20.5},
+        {id: 'e4', ItemDate: new Date(2022, 1, 24), Title: 'Item 4', Price: 23.7}
+    ];
+
+    return (
+        <div>
+            {Expenses.map(exp => 
+              <ExpenseItem 
+                    ItemDate = {exp.ItemDate}
+                    Title = {exp.Title}
+                    Price = {exp.Price}
+                />  
+            )}
+        </div>
+    );
 }
 
 export default App;
--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,11 +1,18 @@
 import './ExpenseItem.css';
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+	const month = props.ItemDate.toLocaleString('en-US', { month: 'long' });
+  	const day = props.ItemDate.toLocaleString('en-US', { day: '2-digit' });
+  	const year = props.ItemDate.getFullYear();
 	return (
 		<div className="expense-item">
-			<div>March 28th 2021</div>
+			<div className="expense-date">
+				<div className="expense-date__month">{month}</div>
+				<div className="expense-date__year">{year}</div>
+				<div className="expense-date__day">{day}</div>
+			</div>
 			<div className="expense-item__description">
-				<h2>Item 1</h2>
-				<p className="expense-item__price">$ 19.4</p>
+				<h2>{props.Title}</h2>
+				<p className="expense-item__price">$ {props.Price}</p>
 			</div>
 		</div>
 	)
